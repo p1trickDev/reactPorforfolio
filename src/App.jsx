f88f50adb32136278c1095442cd1b7a6774731dd
p1trickDev <p1trickdev@gmail.com>
2025-02-19 01:11:32 +0800

    last before sleep

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AboutMe from "./components/AboutMe";
-import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
+    <>
       <Navbar />
       <Hero />
       <AboutMe />
-    </div>
+    </>
   );
 };
 
