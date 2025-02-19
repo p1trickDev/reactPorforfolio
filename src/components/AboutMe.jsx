abd136fc803cf6323f0f5ac95bdde9cfe8d8c816
p1trickDev <p1trickdev@gmail.com>
2025-02-19 12:47:35 +0800

    make the motto ittalic

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,12 +5,12 @@ const AboutMe = () => {
   return (
     <section id="about" className="about-me">
       <div className="aboutme-card">
-        
         <div className="card-content">
           <p>
-            Hello! I am an full-stack developer with experience in various tools and modern technologies.
-            Simplicity and Efficiency are my core values. 
-            My motto: "Create like a god, command like a king, work like a slave."
+            Hello! I am an full-stack developer with experience in various tools
+            and modern technologies. Simplicity and Efficiency are my core
+            values. My motto:{" "}
+            <i>Create like a god, command like a king, work like a slave.</i>
           </p>
         </div>
       </div>
